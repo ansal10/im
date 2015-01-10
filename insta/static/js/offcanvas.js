826890e1198d1835e4e413f176bf82c61491b67a
ANAS MD <amd@groupon.com>
2015-01-10 18:42:08 +0530

    added login and register tempelate

---
+++ insta/static/js/offcanvas.js
@@ -0,0 +1,8 @@
+/**
+ * Created by amd on 12/23/14.
+ */
+$(document).ready(function () {
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('.row-offcanvas').toggleClass('active')
+  });
+});
--- insta/urls.py
+++ insta/urls.py
@@ -2,16 +2,19 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-
-import hello.views
+from hw import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gettingstarted.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', views.index, name='index'),
+    url(r'^index$' , views.index, name='index'),
+    url(r'^db$', views.db, name='db'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^recharge$', views.recharge, name='recharge'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
